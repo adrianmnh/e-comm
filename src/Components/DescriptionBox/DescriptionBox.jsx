a130b3b3299de761d6fca2cf6d3ee9171d35712c
Adrian Legion <nlawliet6@gmail.com>
2024-01-10 21:59:24 -0500

    ProductDisplay, Breadcrums, DescriptionBox components for Product page

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+	<div className="descriptionbox">
+		<div className="descriptionbox-navigator">
+			<div className="descriptionbox-nav-box">Description</div>
+			<div className="descriptionbox-nav-box fade">Reviews (122)</div>
+		</div>
+		<div className="descriptionbox-description">
+			<p>An e-commerce website is an online platform that facilitates online transactions of goods and services through electronic means. It allows customers to browse and purchase products or services from the comfort of their own homes. E-commerce websites typically include features such as product listings, shopping carts, secure payment gateways, and order tracking.</p>
+		</div>
+	  
+	</div>
+  )
+}
+
+export default DescriptionBox
