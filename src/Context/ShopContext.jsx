e23e423afa0c7ccb97e1f9f30fe392a6a5f07042
Adrian Legion <nlawliet6@gmail.com>
2024-02-09 22:58:49 -0500

    feat: Add API call to fetch all products and update stateAdded an API call to fetch all products from the server and update the state with the received data

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,12 @@
-import React, { createContext, useState } from "react";
-import all_product from "../Components/Assets/all_product";
+import React, { createContext, useState, useEffect } from "react";
+// import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
 	let cart = {};
-	for (let i = 0; i < all_product.length; i++) {
+	// for (let i = 0; i < all_product.length; i++) {
+	for (let i = 0; i < 30+1; i++) {
 		cart[i] = 0;
 	}
 	return cart;
@@ -13,7 +14,48 @@ const getDefaultCart = () => {
 
 const ShopContextProvider = (props) => {
 
+	const apiUrl = process.env.REACT_APP_API_ENDPOINT;
+
+	const [allProduct, setAllProduct] = useState([]);
+	const [prods, setProds] = useState([]);
 	const [cartItems, setCartItems] = useState(getDefaultCart());
+
+	useEffect(() => {
+		fetch(`${apiUrl}/all_product`)
+		.then( res => {
+			if (!res.ok) {
+				if (res.state >= 500){
+					throw new Error('Server Error');
+				}
+				else {
+					throw new Error('Unknown Error');
+				}
+			}
+			return res.json();
+		}).then( data => {
+			setAllProduct(data.all_product)
+			return data.all_product;
+
+			// further processing of data
+		}).then( (all_product) => {
+
+			setProds(all_product.map( (product) => ({
+				id: product.id,
+				name: product.name,
+				category: product.category
+			})));
+
+		}).catch( error => {
+			console.log(error);
+		})
+	}, [])
+
+	useEffect(() => {
+		console.log(prods)
+		prods.length === 0 ? console.log('No products found') : console.log('Products found');
+	}, [prods])
+
+
 
 	const addToCart = (itemId) => {
 		setCartItems((prev) => ({...prev, [itemId] : prev[itemId] + 1}))
@@ -32,7 +74,7 @@ const ShopContextProvider = (props) => {
 		let totalAmount = 0;
 		for (const item in cartItems){
 			if (cartItems[item] > 0) {
-				let itemInfo = all_product.find((product) => product.id === Number(item));
+				let itemInfo = allProduct.find((product) => product.id === Number(item));
 				// At this point, itemInfo is defined, so its properties can be safely accessed
 				totalAmount += itemInfo.new_price * cartItems[item];
 			}
@@ -50,7 +92,7 @@ const ShopContextProvider = (props) => {
 		return totalItems;
 	}
 
-	const contextValue = {all_product, cartItems, addToCart, removeFromCart, removeItemFromCart, getTotalCartAmount, getTotalCartItems};
+	const contextValue = {allProduct, cartItems, apiUrl, addToCart, removeFromCart, removeItemFromCart, getTotalCartAmount, getTotalCartItems};
 
 	return (
 		<ShopContext.Provider value={contextValue}>
