23c6b90386ee552835027968dc800ca6c72425aa
Adrian Legion <nlawliet6@gmail.com>
2024-02-19 23:15:20 -0500

    style: Update CSS width and font sizes, capitalize item name, and adjust JSX formatting in Item component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,18 +6,45 @@ const Item = (props) => {
 	const snapToTop = () => {
 		window.scrollTo(0, 0)
 	}
+
+	console.log(props)
+
+	const linkName = props.name.replace(' ', '-').toLowerCase()
+	const productCategory = props.category
+	// const productName = props.name.toCamelCase()
+
+	const prices = () => {
+
+		return (
+			<div className="item-prices">
+			{ !props.sale_price ?
+			<div className="item-sale-price">
+				${props.retail_price.toFixed(2)}
+			</div>
+			:
+			<>
+			<div className="item-sale-price">
+				${props.sale_price.toFixed(2)}
+			</div>
+			<div className="item-retail-price">
+				${props.retail_price.toFixed(2)}
+			</div>
+			</>
+			}
+		</div>
+		)
+
+	}
+
+	// console.log(props.keyy)
 	return (
 		<div className="item">
-			<Link to={`/product/${props.id}`}><img onClick={snapToTop} src={props.image} alt="" /></Link>
-			<p>{props.name}</p>
-			<div className="item-prices">
-				<div className="item-price-new">
-					${props.new_price}
-				</div>
-				<div className="item-price-old">
-					${props.old_price}
-				</div>
-			</div>
+			{/* <Link to={`/product/${props.id}`}><img onClick={snapToTop} src={props.image} alt="" /></Link> */}
+			{/* <Link to={`/product/${linkName}`}><img onClick={snapToTop} src={props.image} alt="" /></Link> */}
+			<Link to={`/shop/${productCategory}/${linkName}`}><img onClick={snapToTop} src={props.image} alt="" /></Link>
+			<p className='item-name'>{props.name}</p>
+			{prices()}
+
 		</div>
 	)
 }
