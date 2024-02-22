0b5fd819bd3c5c06783291f0e26d9918737e7453
Adrian Legion <nlawliet6@gmail.com>
2024-02-22 17:10:34 -0500

    feat: Add signupApi and logout function to AuthContext.jsx

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -8,8 +8,6 @@ const AuthContextProvider = (props) => {
 	const { apiUrl } = useContext(ShopContext);
 
 	const loginApi = async ( formData ) => {
-
-
 		return await fetch(`${apiUrl}/login`, {
 			method: 'POST',
 			headers: {
@@ -40,9 +38,45 @@ const AuthContextProvider = (props) => {
 		})
 	}
 
+	const signupApi = async (formData) => {
+		return await fetch(`${apiUrl}/signup`, {
+
+			// Now you can use the `homepage` variable in your component. , {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(formData)
+		}).then(res => {
+			if (!res.ok) {
+				if (res.status === 401) { // Unauthorized
+					throw new Error('Incorrect password');
+				} else {
+					throw new Error('Network error. Failed to Signup');
+				}
+			}
+			return res.json()
+		}).then(data => {
+			return data;
+		}).catch(error => {
+			console.log(error)
+			return { success: false, message: error.message };
+		})
+	}
+
+	const logout = () => {
+		localStorage.removeItem('x-access-token');
+		localStorage.removeItem('cart');
+		setTimeout(() => {
+			window.location.replace('/')
+		}, 1000)
+
+	}
+
 
 	const contextValue = {
-		loginApi
+		loginApi, signupApi, logout
 	}
 
 	return (
