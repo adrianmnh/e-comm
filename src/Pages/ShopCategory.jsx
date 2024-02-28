03aac8235065f24c48ea41d67e1ba8c51b3e03d7
Adrian Legion <nlawliet6@gmail.com>
2024-02-27 23:55:45 -0500

    perf: Shop by category filter products improved allProducts no longer an array(ShopContext) -> map to improve performance using product id rather than find iterating over whole array

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useMemo } from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item'
 
 const ShopCategory = (props) => {
-	const {all_product} = useContext(ShopContext)
+
+	const { allProduct, linkNameMap } = useContext(ShopContext)
+
+	const filteredProducts = useMemo(() => {
+		if (!allProduct) return [];
+
+		return Array.from(allProduct.entries())
+			.filter(([key, item]) => props.category === item.category);
+	}, [allProduct, props.category]);
 
 	return (
 		<div className="shop-category">
-			<img className="shopcategory-banner" src={props.banner} alt="" />
+			{/* <img className="shopcategory-banner" src={props.banner} alt="" /> */}
 			<div className="shopcategory-indexSort">
 				<p>
 					<span>Showing 1-12</span> out of 36 products
@@ -19,14 +27,32 @@ const ShopCategory = (props) => {
 				</div>
 			</div>
 			<div className="shopcategory-products">
-				{all_product.map((item, i) => {
-					if(props.category === item.category){
-						return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-					} 
-					else {
-						return null
-					}
+
+				{filteredProducts.map(([key, item]) => {
+					return (
+						<Item
+							key={key}
+							id={item.id}
+							name={item.name}
+							image={item.image}
+							category={item.category}
+							retail_price={item.retail_price}
+							sale_price={item.sale_price ? item.sale_price : null}
+							linkName={item.linkName}
+							// sizes
+						/>
+					);
 				})}
+
+				{/* {	productsByCategory.length > 0 &&
+					productsByCategory.map((item, i) => {
+						return <Item key={i} id={item.id} name={item.name} image={item.image} category={item.category}
+							retail_price={item.retail_price} sale_price={item.sale_price ? item.sale_price : null}
+							linkName={item.linkName} sizes />
+					})
+				} */}
+
+
 			</div>
 			<div className="shopcategory-loadmore">
 				Explore More
