45af48b5282bbf2763e088cfba4f798d2c59fe41
Adrian Legion <nlawliet6@gmail.com>
2024-03-11 22:32:31 -0400

    feat: Add Account page component with CSS styling

---
+++ src/Pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSS/Account.css'
+
+const Account = () => {
+  return (
+	<div className='account-main-wrapper'>
+		account details...
+	</div>
+  )
+}
+
+export default Account
